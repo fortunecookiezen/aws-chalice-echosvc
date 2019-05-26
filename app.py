23f82ad12d37740bf10e76aa61ccf3fd539f7b3e
jamesp <phillips.james@gmail.com>
2019-05-26 10:27:16 -0500

    initial import

---
+++ app.py
@@ -0,0 +1,22 @@
+from chalice import Chalice, Response
+from chalicelib import URL
+app = Chalice(app_name='echosvc')
+
+@app.route('/')
+def index():
+    return { 'hello': 'world'}
+
+@app.route('/echo')
+def echo():
+    ctx = app.current_request.to_dict()
+    return { 'source' : (ctx['context']['identity']['sourceIp']) }
+
+@app.route('/hello/{name}')
+def hello(name):
+    return { 'hello' : name }
+
+@app.route('/rick')
+def rick():
+    return Response(body='',
+                    status_code=301,
+                    headers={'Location': URL })
